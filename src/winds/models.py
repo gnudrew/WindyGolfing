d1d1e8a7d9617a27d01247c05f53d9df56526e0d
gnudrew <gnudrew@gmail.com>
2022-12-04 18:03:33 -0600

    refinements to Winds models

--- src/winds/models.py
+++ src/winds/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
@@ -12,23 +14,23 @@ class Timestamped(models.Model):
     class Meta:
         abstract = True
 
-def set_triple_0():
+def get_triple_0():
     return [0, 0, 0]
 class WindGeneratorParameters(Timestamped):
     """Parameters used by the generator to generate wind speed trajectories per spatial dimension over time."""
     # flags to indicate generator
-    is_windless = models.BooleanField()
-    is_constant = models.BooleanField()
-    is_oscillatory = models.BooleanField()
-    is_lorenz = models.BooleanField()
+    is_windless = models.BooleanField(default=False)
+    is_constant = models.BooleanField(default=False)
+    is_oscillatory = models.BooleanField(default=False)
+    is_lorenz = models.BooleanField(default=False)
 
     # Constant
-    base_speed = ArrayField(models.FloatField(), max_length=3, default=set_triple_0) # [x,y,z]
+    base_speed = ArrayField(models.FloatField(), max_length=3, default=get_triple_0) # [x,y,z]
 
     # Oscillatory --> speed = base_speed + amplitude * cos(2𝜋*frequency*t + phase_offset)   ... for each of [x,y,z] directions
-    frequency = ArrayField(models.FloatField(), max_length=3, default=set_triple_0) # [x,y,z]
-    amplitude = ArrayField(models.FloatField(), max_length=3, default=set_triple_0) # [x,y,z]
-    phase_offset = ArrayField(models.FloatField(), max_length=3, default=set_triple_0) # [x,y,z]
+    frequency = ArrayField(models.FloatField(), max_length=3, default=get_triple_0) # [x,y,z]
+    amplitude = ArrayField(models.FloatField(), max_length=3, default=get_triple_0) # [x,y,z]
+    phase_offset = ArrayField(models.FloatField(), max_length=3, default=get_triple_0) # [x,y,z]
 
     # Lorenz
     rho = models.FloatField(null=True, default=0)
@@ -38,9 +40,10 @@ class WindGeneratorParameters(Timestamped):
 
 class WindSpacetime(Timestamped):
     """Trajectories of wind speed per spatial dimension (x,y,z) over time (t) --> Table contains meta data, blob contains the actual time-trajectory data."""
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     generator_name = models.CharField(max_length=30, choices=WIND_GENERATOR_NAMES)
-    generator_parameters = models.OneToOneField(WindGeneratorParameters, on_delete=models.CASCADE)
+    generator_parameters = models.OneToOneField(WindGeneratorParameters, on_delete=models.CASCADE, null=True)
     duration = models.FloatField() # in seconds
 
     blob_filename = models.CharField(max_length=50, null=True) # filenames will be uuid plus extension... <uuid>.pkl ... so we expect 40 or so characters
+
