16d3f7fecd452bef798c9c28a26c14506729ac26
gnudrew <gnudrew@gmail.com>
2022-12-09 21:52:06 -0600

    Add existence check while POSTing to WindGeneratorParametersViewSet

--- src/winds/views.py
+++ src/winds/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import WindGeneratorParameters
 from .serializers import WindGeneratorParametersSerializer
@@ -8,3 +9,14 @@ from .serializers import WindGeneratorParametersSerializer
 class WindGeneratorParametersViewSet(ModelViewSet):
     queryset = WindGeneratorParameters.objects.all()
     serializer_class = WindGeneratorParametersSerializer
+
+    def create(self, request, *args, **kwargs):
+        # check existence
+        serializer = self.get_serializer(data=self.request.data)
+        if serializer.is_valid():
+            qs = self.get_queryset().filter(**serializer.validated_data)
+            if qs.count() != 0:
+                print("This parameter set already exists.")
+                return Response("This parameter set already exists.", status=409)
+
+        return super().create(request, *args, **kwargs)
