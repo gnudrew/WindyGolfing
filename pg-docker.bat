346a9e9b2ef43e3c511ed952bae5a800639f7ead
gnudrew <gnudrew@gmail.com>
2022-12-11 16:48:07 -0600

    Fixed postgres docker issue, syntax setting ENV vars

--- pg-docker.bat
+++ pg-docker.bat
@@ -1,1 +1,1 @@
-docker run --name postgres -e POSTGRES_PASSWORD=password,POSTGRES_USER=admin,POSTGRES_DB=postgres,POSTGRES_PORT=5432 -p 5432:5432 -d postgres
+docker run --name postgres -e POSTGRES_PASSWORD=password -e POSTGRES_USER=postgres -e POSTGRES_DB=postgres -e POSTGRES_PORT=5432 -p 5432:5432 -d postgres
