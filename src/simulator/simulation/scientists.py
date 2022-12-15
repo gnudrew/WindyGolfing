d2aebf152af3f8ac2c13c6ec4eba077b6af72dd5
gnudrew <gnudrew@gmail.com>
2022-12-14 23:04:31 -0600

    Outline methods in Scientist and SimTrialRunner to do a Monte Carlo experiment

--- src/simulator/simulation/scientists.py
+++ src/simulator/simulation/scientists.py
@@ -1,5 +1,149 @@
 """Scientist classes, which conduct experiments and such"""
 
-class ArnoldPalmer:
-    """Conducts Monte Carlo experiments, sampling many SimTrials for a given parameter set"""
-    pass
+from .probabilities import NormalProbGen, UniformProbGen, LogNormalProbGen
+from .sim import SimTrialRunner
+from commons.wranglers import BlobWrangler
+from winds.models import WindSpacetime
+
+
+
+class Scientist:
+    """ Conducts Monte Carlo experiments, sampling many SimTrials for a given parameter set """
+    required_keys_params = [
+        'windspacetime_id',
+        'num_trials',
+        'prob_speed_fn_name',
+        'max_initial_speed',
+        'prob_speed_center',
+        'prob_speed_spread',
+        'prob_timing_fn_name',
+        'max_wait_time',
+        'prob_timing_center',
+        'prob_timing_spread',
+        'prob_aiming_fn_name',
+        'prob_aiming_geometry',
+        'prob_aiming_X1_min',
+        'prob_aiming_X1_max',
+        'prob_aiming_X1_center',
+        'prob_aiming_X1_spread',
+        'prob_aiming_X2_min',
+        'prob_aiming_X2_max',
+        'prob_aiming_X2_center',
+        'prob_aiming_X2_spread',
+        'prob_aiming_X3_min',
+        'prob_aiming_X3_max',
+        'prob_aiming_X3_center',
+        'prob_aiming_X3_spread',
+        'timestep',
+    ]
+    optional_keys_params = [
+        'g',
+        'm',
+        'drag_coef',
+    ]
+
+    def __init__(self, params):
+        """
+        Parameters:
+        -------
+        df_windspacetime: pd.DataFrame
+            ...
+        params: dict
+            ...
+            keys must include: [
+                'windspacetime_id',
+                'prob_speed_fn_name',
+                'max_initial_speed',
+                'prob_speed_center',
+                'prob_speed_spread',
+                'prob_timing_fn_name',
+                'max_wait_time',
+                'prob_timing_center',
+                'prob_timing_spread',
+                'prob_aiming_fn_name',
+                'prob_aiming_geometry',
+                'prob_aiming_X1_min',
+                'prob_aiming_X1_max',
+                'prob_aiming_X1_center',
+                'prob_aiming_X1_spread',
+                'prob_aiming_X2_min',
+                'prob_aiming_X2_max',
+                'prob_aiming_X2_center',
+                'prob_aiming_X2_spread',
+                'prob_aiming_X3_min',
+                'prob_aiming_X3_max',
+                'prob_aiming_X3_center',
+                'prob_aiming_X3_spread',
+                'timestep',
+            ],
+            keys can optionally include: [
+                'g',
+                'm',
+                'drag_coef,
+            ]
+        
+        """
+        # assign params
+        self._check_params(params)
+        self.params = params
+
+        # load winds
+        self.load_windspacetime(self.params)
+
+        # build probability functions
+        self.gen_prob_fns()
+
+    def _check_params(self, params):
+        """Checks that supplied params meet requirements."""
+        # check for required and optional params
+        if not set(self.required_keys_params).issubset(set(params.keys())):
+            raise AssertionError('Required params are missing.')        
+
+    def load_windspacetime(self,):
+        id = self.params['windspacetime_id']
+        o = WindSpacetime.objects.get(pk=id)
+        self.df_windspacetime = BlobWrangler().read_blob(o)
+        self.arr_windspacetime = self.df_windspacetime.to_numpy()
+
+    def gen_prob_fns(self,):
+        prob_fn_timing = None
+        prob_fn_aiming = None
+        prob_fn_speed = None
+
+        self.prob_fns = {
+            'timing': prob_fn_timing,
+            'aiming': prob_fn_aiming,
+            'speed': prob_fn_speed,
+        }
+
+    def run_experiment(self,):
+        """
+        Run the experiment
+        
+        Returns:
+        -------
+        simtrial_ids: list
+            list of id's for the sim trials created during this experiment
+        """
+        timestep = self.params['timestep']
+        N = self.params['num_trials']
+        
+        # do the trials
+        simtrial_ids = []
+        for n in range(N):
+            # choose time
+            t_initial = None
+            # choose aim
+            ## choose geometry coordinates
+            ## convert to unit vector
+            # choose speed
+
+            # set initial velocity
+            v_initial = None
+            
+            # run a trial
+            runner = SimTrialRunner(t_initial, v_initial, self.arr_windspacetime, timestep)
+            id = runner.run()
+            simtrial_ids.append(id)
+        
+        return simtrial_ids
--- src/simulator/simulation/sim.py
+++ src/simulator/simulation/sim.py
@@ -2,4 +2,35 @@
 
 class SimTrialRunner:
     """Takes raw inputs and simulates a single ball trajectory"""
-    pass
+    def __init__(self, t_initial, v_initial, arr_windspacetime, timestep):
+        """
+        Parameters:
+        -------
+        t_initial: float
+            The initial time of the sim, when the ball is hit
+        v_initial: np.array
+            The initial velocity vector of the ball
+        arr_windspacetime: np.array
+            The wind velocity data
+        timestep: float
+            The time between each row of wind speeds, and between simulation compute steps
+        """
+        self.t_initial = t_initial
+        self.v_initial = v_initial
+        self.arr_windspacetime = arr_windspacetime
+        self.timestep = timestep
+
+    def run(self,):
+        # iterate till ball hits ground
+        ball_hit_ground = False
+        while not ball_hit_ground:
+            pass
+
+        # store the trajectory data and return simtrial id
+        simtrial_id = self.store_trial()
+        return simtrial_id
+
+    def store_trial(self,):
+        """Store blob and simtrial obj and return simtrial_id"""
+        simtrial_id = None
+        return simtrial_id
