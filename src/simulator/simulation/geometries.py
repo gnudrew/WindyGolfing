16a88bebba4c5f8b6da59b03504760f835d39da7
gnudrew <gnudrew@gmail.com>
2022-12-22 11:39:52 -0600

    Added SphericalGeometry.get_unit_vector()

--- src/simulator/simulation/geometries.py
+++ src/simulator/simulation/geometries.py
@@ -1,5 +1,7 @@
 """Geometries whose dimensions can be sampled by probability functions"""
 
+import numpy as np
+
 class Geometry:
     """Base class"""
     pass
@@ -10,7 +12,26 @@ class EulerAnglesGeometry(Geometry):
 
 class SphericalGeometry(Geometry):
     """Spherical coordinates geometry"""
-    pass
+    def __init__(self, theta, phi, r):
+        """
+        Parameters:
+        -------
+        theta: float
+            Spherical angle, theta
+        phi: float
+            Spherical angle, phi
+        r: float
+            Spherical radius, r
+        """
+        self.theta = theta
+        self.phi = phi
+        self.r = r
+
+    def get_unit_vector(self,):
+        x = np.sin(self.phi)*np.cos(self.theta)
+        y = np.sin(self.phi)*np.sin(self.theta)
+        z = np.cos(self.phi)
+        return np.array([x,y,z])
 
 class CylindricalGeometry(Geometry):
     """Cylindrical coordinates geometry"""
