a24ea82a4dc021a5af95021e6bc32771a4100491
gnudrew <gnudrew@gmail.com>
2022-12-26 17:00:20 -0600

    prev commit

--- src/winds/serializers.py
+++ src/winds/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 
 from .models import WindGenParams, WindSpacetime
 
-class WindGenParamsSerializer(serializers.HyperlinkedModelSerializer):
+class WindGenParamsSerializer(serializers.ModelSerializer):
     class Meta:
         model = WindGenParams
         fields = '__all__'
 
-class WindSpacetimeSerializer(serializers.HyperlinkedModelSerializer):
+class WindSpacetimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = WindSpacetime
         exclude = [
